65e92f4edf34c16ed7cc653a124814fd4cc14240
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-19 22:11:12 +0500

    Initial Commit

---
+++ DAY 9/String_Slicing_and_Operations.py
@@ -0,0 +1,24 @@
+names = "Awais,Ali,Ahad"
+print(names[0:5])
+
+
+names = "Awais, Ali, Ahad"
+print(len(names))
+
+
+fruit = "Apple"
+len1 = len(fruit)
+print("The length of",len1 ,"letter words")
+
+
+car = "Mercedes"
+print(car[:5])
+
+
+car = "Mercedes"
+print(car[0:-3])
+
+
+car = "Mercedes"
+print(car[-3:-1]) # 8-3=5 , 8-1 =7  5:7 {de}will print
+
