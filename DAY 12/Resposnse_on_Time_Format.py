d17ab51b07359d261ae295261130396df828e918
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-21 23:10:26 +0500

    Initial commit

---
+++ DAY 12/Resposnse_on_Time_Format.py
@@ -0,0 +1,10 @@
+time = int(input("Enter Time in 24 hours format: "))
+if(time==5 or time==6 or time==7 or time==8 or time==9 or time==10):
+    print("Good Morning Sir")
+elif(time==11 or time==12 or time==13 or time==14
+     or time==15):
+    print("Good Afternoon Sir")
+elif(time==16 or time==17 or time==18 or time==19 ):
+    print("Good Evening Sir")
+else:
+    print("Good Night Sir")
