b14abf0a91561cbdd4608c52058cc3e99b5f99aa
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-21 23:41:09 +0500

    Add more Systematic Format

--- DAY 12/Resposnse_on_Time_Format.py
+++ DAY 12/Resposnse_on_Time_Format.py
@@ -1,10 +1,48 @@
-time = int(input("Enter Time in 24 hours format: "))
-if(time==5 or time==6 or time==7 or time==8 or time==9 or time==10):
-    print("Good Morning Sir")
-elif(time==11 or time==12 or time==13 or time==14
-     or time==15):
-    print("Good Afternoon Sir")
-elif(time==16 or time==17 or time==18 or time==19 ):
-    print("Good Evening Sir")
+# Noob Beginner
+
+# time = int(input("Enter Time in 24 hours format: "))
+# if(time==5 or time==6 or time==7 or time==8 or time==9 or time==10):
+#     print("Good Morning Sir")
+# elif(time==11 or time==12 or time==13 or time==14
+#      or time==15):
+#     print("Good Afternoon Sir")
+# elif(time==16 or time==17 or time==18 or time==19 ):
+#     print("Good Evening Sir")
+# else:
+#     print("Good Night Sir")
+
+# using easy format
+
+# import time
+
+# print(time.ctime())
+
+# another method 
+
+
+import time
+
+print("Enter Time in hours: ")
+hour = int(input(time)) 
+
+
+if 5 <= hour < 12:
+    print("Good Morning!")
+elif 12 <= hour < 17:
+    print("Good Afternoon!")
+elif 17 <= hour < 22:
+    print("Good Evening!")
 else:
-    print("Good Night Sir")
+    print("Good Night!")
+
+
+
+
+
+
+
+
+
+
+
+
