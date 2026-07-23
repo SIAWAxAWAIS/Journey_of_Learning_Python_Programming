f36cfe46d6c08257aa29a2be7b2b4f344816882d
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-23 20:27:20 +0500

    Create while_loop.py

---
+++ DAY 15/while_loop.py
@@ -0,0 +1,32 @@
+# i =0
+# while(i<=3):
+#     print(i)
+#     i= i+1
+# print("Done")
+
+
+# i =0
+# while(i<=50):
+#     i = int(input("Enter a Number: "))
+#     print(i)
+# print("The End")
+
+
+
+ # decrementing while loop
+
+# x =10
+# while(x>0):
+#     print(x)
+#     x =x-1
+# else:
+#     print("Are you Okey")
+
+# emulation of do while loop
+
+while True:
+    i =input("Enter a Negative Number to Break the loop:")
+    number = int(i)
+
+    if number<=0:
+        break
