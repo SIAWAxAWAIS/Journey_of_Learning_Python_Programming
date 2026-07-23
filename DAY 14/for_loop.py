bcb714a2b71968757bf3492ee953d5b9328b2ad9
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-23 15:18:29 +0500

    Initial commit

---
+++ DAY 14/for_loop.py
@@ -0,0 +1,39 @@
+# iteration for a string
+
+# name= "Abdullah"
+# for i in name:
+#     print(i)
+#     if (i=="u"):
+#         print("App ka dost")
+
+ 
+# iteration for a list
+
+# fruits = ["Apple","Mango","Gauva","Orange"]
+# for fruit in fruits:
+#     print(fruit)
+#     for i in fruit:
+#         print(i)
+
+#iteration for a range
+
+# for x in range(5):
+#     print(x+1)
+
+# for x in range(1,20000):
+#     print(x)
+
+
+# iteration for step
+
+for x in range(10,0,-3):
+    print(x)
+
+
+for x in range(3,6,9):
+    print(x)
+
+
+str = ['a','b','c','d','e','f']
+for s in str[::4]:
+    print(s)
