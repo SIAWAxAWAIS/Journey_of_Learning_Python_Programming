a9a1be834784148d7d6e6bcfa64257ab21144266
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-24 15:33:54 +0500

    Initial commit

---
+++ DAY 16/continue_and_break_statement.py
@@ -0,0 +1,32 @@
+# Table of 7
+
+# for i in range(1,11):
+#     print("7 x", i, "=" ,7*i)
+
+
+# Break Statement
+
+# for i in range(1,11):
+#      print("5 x", i, "=" ,5*i)
+#      if(i==7):
+#          break
+
+
+# continue statement
+
+# for i in range(1,11):
+#     if(i==5):
+#         print("Everything is fine")
+#         continue
+#     print("5 x" ,i, "=",5*i)
+
+
+# emulation of do while loop with break
+
+i =0
+while True:
+    print(i)
+    i = i+1
+    if (i%100 ==0):
+        break
+    
