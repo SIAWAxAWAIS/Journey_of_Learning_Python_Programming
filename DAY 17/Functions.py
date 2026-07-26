4b62e7ea2e29dfb1df300f7c76364d33374ecf9e
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-26 16:00:42 +0500

    Initial commit

---
+++ DAY 17/Functions.py
@@ -0,0 +1,76 @@
+# Without function
+
+# a =20
+# b =30
+
+# average = (a+b)/2
+# print(average)
+
+# c =50
+# d =100
+
+# avg =  (c+d)/2
+# print(avg)
+
+
+
+# with function
+
+# def calculateAverage(a, b):
+#     mean = (a+b)/(2)
+#     print(mean)
+
+# a = 20
+# b = 30
+# calculateAverage(a, b)
+
+# c =50
+# d =100
+# calculateAverage(c,d)
+  
+
+
+
+# Odd or Even without function
+
+# a =6
+# if (a%2==0):
+#     print("The number is Even")
+# else:
+#     print("The number is Odd")
+
+
+# b=9
+# if (b%2==0):
+#     print("The number is Even")
+# else:
+#     print("The number is Odd")
+
+
+  
+# Odd or Even with function
+
+def isEvenOrOdd (a):
+    if (a%2==0):
+        print("The number is Even")
+    else:
+        print("The number is Odd")
+
+# to pass the function
+def isLessOrGreater(a ,b):
+    pass
+
+a=103
+isEvenOrOdd(a)
+
+b =76
+isEvenOrOdd(b)
+
+
+
+
+# for string
+def name(Name):
+    print("Hello",Name )
+
+name("Ali")
