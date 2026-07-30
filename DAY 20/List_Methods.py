c23358d39bf83c8f6ad57b7c516a6421490af8d8
AWAIS MIRZA <mawais245gb@gmail.com>
2026-07-30 21:52:59 +0500

    Initial commit

---
+++ DAY 20/List_Methods.py
@@ -0,0 +1,31 @@
+l = [5,4,10,77,34]
+# l.append(14)
+# l.sort()
+# l.sort(reverse=True)
+# l.reverse()
+# print(l.index(10))
+# print(l.count(5))
+# k = l
+# k[0]= 1
+# print(l)
+
+
+# copy funtion
+# k = l.copy()
+# k[0] = 66
+# print(l)
+
+# insert method
+# l.insert(1 ,99)
+# print(l)
+
+# extend
+# m = [100,200 ,399]
+# l.extend(m)
+# print(l)
+
+# concatinating two lists
+m = [66,77,88]
+k = l+m
+print(k)
+
