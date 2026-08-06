3596704762ee9549d2377dff8483d0ca35664b1f
AWAIS MIRZA <mawais245gb@gmail.com>
2026-08-06 19:32:36 +0500

    Initial commit

---
+++ DAY 29/dict_methods.py
@@ -0,0 +1,11 @@
+empId1 = {242929:95 , 528251:90 , 497826:75}
+
+empId2 = {766438:78 , 756386:86}
+
+# empId1.update(empId2)
+# empId1.clear()
+# empId1.pop(242929)
+# empId1.popitem()
+# del empId1 ---> delete dictionary and shows an error if the deleted dictionary prints
+# del empId1[242929]
+print(empId1)
