f51c038aea594bec8d9167b5e5d203928fd5024d
AWAIS MIRZA <mawais245gb@gmail.com>
2026-08-07 15:50:47 +0500

    Initial commit

---
+++ DAY 30/for_else.py
@@ -0,0 +1,19 @@
+for i in []:
+    print(i)
+else:
+    print("Empty")
+
+
+for i in range(5):
+    print(i)
+    # if i == 4:
+    #     break  ---> program ends
+else:
+    print("Dynamite")
+
+
+for k in range(5):
+    print("Iteration num {} in for loop".format(k+1))
+else:
+    print("else block in for loop")
+
