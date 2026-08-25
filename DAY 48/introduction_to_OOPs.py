b0cacad5189e4951211fd47cf45d22dceed28da2
AWAIS MIRZA <mawais245gb@gmail.com>
2026-08-25 15:17:38 +0500

    Initial commit

---
+++ DAY 48/introduction_to_OOPs.py
@@ -0,0 +1,24 @@
+def greet():
+    print("Hello")
+
+greet()
+
+
+
+# sales1 = "$3000"
+# revenue1 = 10000
+# ad1 = 1000
+
+# sales2 = "$3000"
+# revenue2 = 10000              ---> Instead of doing this we can use OOPs
+# ad2 = 1000
+
+# sales3 = "$3000"
+# revenue3 = 10000
+# ad3 = 1000
+
+
+# Inheritance
+# Polymorphism
+# Encapsulation
+# Abstraction
